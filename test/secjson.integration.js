de7a157c1f8f397d2f4f6d0075070b2541c5526b
Tiago Santos <tiagomistral21@gmail.com>
2015-03-16 17:05:51 +0000

    Sample mocha test

---
+++ test/secjson.integration.js
@@ -0,0 +1,13 @@
+var assert = require('assert'),
+    fs = require('fs'),
+    xmlenc = require('../lib');
+
+var crypto = require('crypto');
+
+describe('integration', function() {
+
+  it('should decrypt assertion with aes128', function (done) {
+    done();
+  });
+
+});
