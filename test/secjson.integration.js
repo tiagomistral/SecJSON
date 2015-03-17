c755b34aad549f4d053b776457e4cf0a60354dae
Tiago Santos <tiagomistral21@gmail.com>
2015-03-17 18:13:47 +0000

    First encryption

--- test/secjson.integration.js
+++ test/secjson.integration.js
@@ -7,7 +7,22 @@ var crypto = require('crypto');
 describe('integration', function() {
 
   it('should decrypt assertion with aes128', function (done) {
+  	var payload = 'hello world';
+  	//xmlenc.encrypt(payload);
+	//assert.equal(xmlenc.decrypt(xmlenc.encrypt(payload)), payload);
     done();
+  });
+
+  it('encryptKeyInfoWithScheme', function(done){
+  	var options = {
+      rsa_pub: fs.readFileSync(__dirname + '/test-auth0_rsa.pub'),
+      pem: fs.readFileSync(__dirname + '/test-auth0.pem'),
+      key: fs.readFileSync(__dirname + '/test-auth0.key'),
+      encryptionAlgorithm: 'http://www.w3.org/2001/04/xmlenc#tripledes-cbc',
+      keyEncryptionAlgorighm: 'http://www.w3.org/2001/04/xmlenc#rsa-1_5'
+    };
+    
+  	encryptKeyInfoWithScheme
   });
 
 });
